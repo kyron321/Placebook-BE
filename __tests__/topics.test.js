b4d974f7752b27c22f0e4afc4bccf60b728659b6
kyron321 <kds060902@outlook.com>
2022-12-13 13:56:07 +0000

    GET /api/articles sorted by descending date creation order

--- __tests__/topics.test.js
+++ __tests__/topics.test.js
@@ -1,9 +1,9 @@
-
 const request = require("supertest");
 const app = require("../app");
 const db = require("../db/connection");
 const testData = require("../db/data/test-data");
 const seed = require("../db/seeds/seed");
+require('jest-sorted');
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
@@ -44,22 +44,44 @@ describe("News API", () => {
     test("responds with a status of 200 and returns an array of objects containing the articles", () => {
       return request(app)
         .get("/api/articles")
+        .expect(200)
         .then(({ body }) => {
           const { articles } = body;
           expect(articles.length).toEqual(12);
-          articles.forEach((article) => 
-              expect(article).toMatchObject({
-                author: expect.any(String),
-                title: expect.any(String),
-                article_id: expect.any(Number),
-                topic: expect.any(String),
-                created_at: expect.any(String),
-                votes: expect.any(Number),
-                comment_count: expect.any(String),
-              })
-            );
-          });
+          articles.forEach((article) =>
+            expect(article).toMatchObject({
+              author: expect.any(String),
+              title: expect.any(String),
+              article_id: expect.any(Number),
+              topic: expect.any(String),
+              created_at: expect.any(String),
+              votes: expect.any(Number),
+              comment_count: expect.any(String),
+            })
+          );
+        });
+    });
+
+    test("responds status 200 and returns an array of article objects sorted by a descending date order", () => {
+      return request(app)
+        .get("/api/articles")
+        .expect(200)
+        .then(({ body }) => {
+          const { articles } = body;
+          expect(articles.length).toEqual(12);
+          expect(articles).toBeSortedBy("created_at", { descending: true });
+          articles.forEach((article) =>
+            expect(article).toMatchObject({
+              author: expect.any(String),
+              title: expect.any(String),
+              article_id: expect.any(Number),
+              topic: expect.any(String),
+              created_at: expect.any(String),
+              votes: expect.any(Number),
+              comment_count: expect.any(String),
+            })
+          );
         });
     });
   });
-
+});
--- models/models.js
+++ models/models.js
@@ -14,7 +14,8 @@ exports.selectArticles = () => {
     SELECT articles.*, 
     COUNT(comment_id) AS comment_count FROM articles
     LEFT JOIN comments ON articles.article_id = comments.article_id
-    GROUP BY articles.article_id`)
+    GROUP BY articles.article_id
+    ORDER BY created_at DESC;`)
     .then((result) => {
         return result.rows;
       })
